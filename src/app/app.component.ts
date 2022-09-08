7590b2b5b850e66f34cfcc2095789dac295c0d31
fernando <crosio.fernando@gmail.com>
2022-09-08 17:13:40 -0300

    Creación de la clase empleado.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  titulo = 'Listado de empleados';
+
+  empleados:Empleado[]=[
+
+    new Empleado("Juan", "Diaz", "Presidente", 7500),
+    new Empleado("Ana", "Martin", "Directora", 5500),
+    new Empleado("Maria", "Fernandez", "Jefa sección", 3500),
+    new Empleado("Laura", "Lopez", "Administrativo", 2500),
+  ];
 }
